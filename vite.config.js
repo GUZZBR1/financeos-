272f3d585007eac50f591bdf06828d314c722bf5
GUZZBR1 <gustavobmoreira190508@gmail.com>
2026-03-06 12:10:47 -0300

    Change base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANTE: substitua 'financeos' pelo nome exato do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/financeos/',
+  base: '/financeos-/',
 })
